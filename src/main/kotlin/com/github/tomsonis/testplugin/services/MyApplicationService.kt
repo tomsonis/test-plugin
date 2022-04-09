1577ce40fda0e054d3d5e833feffdba77ea82731
GitHub Action <action@github.com>
2022-04-09 16:59:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/tomsonis/testplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/tomsonis/testplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.tomsonis.testplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tomsonis.testplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/tomsonis/testplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tomsonis/testplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.tomsonis.testplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tomsonis.testplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
